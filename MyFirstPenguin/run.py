681b294c8449fea9ae0620c5f322a64fbc7a5646
Andreas Haugan Aursand <Andreas-MAC@dhcp-10-22-41-7.wlan.ntnu.no>
2018-11-06 19:30:38 +0100

    heyo, er dette riktig?

--- MyFirstPenguin/run.py
+++ MyFirstPenguin/run.py
@@ -64,8 +64,8 @@ def moveTowardsCenterOfMap(body):
     return moveTowardsPoint(body, centerPointX, centerPointY)
 
 def moveToCorner(body):
-    endPointX = math.floor(body.mapWidth)
-    endPointY = math.floor(body.mapHeight)
+    endPointX = math.floor(body["mapWidth"])
+    endPointY = math.floor(body["mapHeight"])
     return moveTowardsPoint(body, endPointX, endPointY)
 
 def chooseAction(body):
